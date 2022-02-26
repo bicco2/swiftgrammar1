950c0e91883c23e29d1723e3b4b622cee56bd62c
jinhobyeong <jhb_0921@naver.com>
2022-02-26 23:59:59 +0900

    day08

--- swiftgrammar1/protocol_1.swift
+++ swiftgrammar1/protocol_1.swift
@@ -1,5 +1,6 @@
 import Foundation
 
+
 // 프로토콜로 약속 한다.(프로토콜 임플리먼트라고 함 )
 protocol Naming {
     //우리는 이런 변수, 메소드를 가지고 있을 겁니다 라고 약속하는 거임
@@ -16,18 +17,26 @@ protocol UserNotifiable : Naming, Aging{
 }
 
 struct exFriend : UserNotifiable {
+    var name: String
+    
+    func getName() -> String {
+        <#code#>
+    }
+    
+    var age: Int
+    
     
 }
 
 
-//struct Friend : Naming{
-//    var name: String
-//
-//    func getName() -> String {
-//        return "my friend : " + self.name
-//    }
-//}
-//
-//var myFriend = Friend(name: "bicco")
-//
-//print(myFriend.getName())
+struct Friend : Naming{
+    var name: String
+
+    func getName() -> String {
+        return "my friend : " + self.name
+    }
+}
+
+var myFriend = Friend(name: "bicco")
+
+print(myFriend.getName())
